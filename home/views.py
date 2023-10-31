50776c735d2a52bd370b37d6e2c4a26b5cfb57e9
ali-hv <alihv5000@gmail.com>
2023-10-31 14:23:12 +0330

    render index.html in home_page view

--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 
 def home_page(request):
-    return HttpResponse("Hi")
+    return render(request, "home/index.html")
