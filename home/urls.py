8e38c0a9a6a76b65e9e3a80b076a1eee7a408669
ali-hv <alihv5000@gmail.com>
2023-10-31 18:14:45 +0330

    add app_name and url names

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import home_page, about_us, contact_us
 
+app_name = "home"
+
 urlpatterns = [
     path("", home_page),
-    path("about-us", about_us),
-    path("contact-us", contact_us),
+    path("about-us", about_us, name="about_us"),
+    path("contact-us", contact_us, name="contact_us"),
 ]
