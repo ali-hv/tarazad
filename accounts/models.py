8b3a6d02c010009eda1c78ee68ada3adbcac3286
ali-hv <alihv5000@gmail.com>
2023-11-01 16:06:54 +0330

    create User(custom user) model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    email_verified = models.BooleanField(default=False)
+    phone_number = models.IntegerField(max_length=12)
+    avatar = models.ImageField(upload_to='avatars/', blank=True)
