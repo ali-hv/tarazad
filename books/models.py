444ceefe92ec4c75ba03160eb6a5c3eff68347af
ali-hv <alihv5000@gmail.com>
2023-11-01 19:03:33 +0330

    create Book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from core.settings import AUTH_USER_MODEL
 
-# Create your models here.
+User = AUTH_USER_MODEL
+
+
+class Book(models.Model):
+    LANGUAGES = (
+        ('en-us', 'English'),
+        ('es-sp', 'Spanish'),
+    )
+
+    STATUS = (
+        ('translated', 'Translated'),
+        ('in-progress', 'In Progress'),
+        ('not-started', 'Not Started')
+    )
+
+    name = models.CharField(max_length=255, verbose_name="Book's Name")
+    detail = models.TextField(verbose_name="Book's Detail")
+    file = models.FileField(verbose_name="Book's File")
+    author = models.CharField(max_length=255, verbose_name="Book's Author")
+    language = models.CharField(choices=LANGUAGES, verbose_name="Book's Language")
+    pages_number = models.PositiveIntegerField(verbose_name="Book's Pages Number")
+    publication_date = models.DateTimeField(verbose_name="Publication Date")
+    translated_date = models.DateTimeField(blank=True, verbose_name="Translated Date")
+    min_translators = models.PositiveIntegerField(verbose_name="Minimum Number of Translators to Start")
+    status = models.CharField(choices=STATUS, verbose_name="Book's Status")
+    translators = models.ManyToManyField(User, verbose_name="Book's Translators", related_name="book_translators")
+
