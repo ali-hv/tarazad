28eca6a07e27f78b777fcb9f13cf63d9883c0292
ali-hv <alihv5000@gmail.com>
2023-11-01 16:12:48 +0330

    change IntegerField to CharField

--- accounts/models.py
+++ accounts/models.py
@@ -4,5 +4,5 @@ from django.db import models
 
 class User(AbstractUser):
     email_verified = models.BooleanField(default=False)
-    phone_number = models.IntegerField(max_length=12)
+    phone_number = models.CharField(max_length=12)
     avatar = models.ImageField(upload_to='avatars/', blank=True)
