020693b3b86e6d19f4473197002c7daaec5e6bff
ali-hv <alihv5000@gmail.com>
2023-11-01 16:26:08 +0330

    register User model and add identity_verified field to User model

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 
 class User(AbstractUser):
+    identity_verified = models.BooleanField(default=False)
     email_verified = models.BooleanField(default=False)
     phone_number = models.CharField(max_length=12)
     avatar = models.ImageField(upload_to='avatars/', blank=True)
