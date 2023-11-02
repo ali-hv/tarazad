4319bbd3ea956e716f43a8bc6381461667778f3a
ali-hv <alihv5000@gmail.com>
2023-11-02 23:05:01 +0330

    complete add_book_to_inprogressbooks signal

--- books/signals/handlers.py
+++ books/signals/handlers.py
@@ -1,7 +1,8 @@
 from django.shortcuts import get_object_or_404
-from books.models import Book, InProgressBook
+from books.models import Book, InProgressBook, Page
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
+import PyPDF2
 
 
 @receiver(pre_save, sender=Book)
@@ -12,7 +13,20 @@ def add_book_to_inprogressbooks(sender, instance: Book, **kwargs):
         previous = Book.objects.get(id=instance.id)
         if previous.status != instance.status:
             if instance.status == "in-progress":
+                pdf_file = open(instance.file.path, 'rb')
+                pdf_reader = PyPDF2.PdfReader(pdf_file)
+
                 book = InProgressBook.objects.create(book=instance)
+                translators = instance.translators.all()
+                number_of_translators = translators.count()
+                pages_number = instance.pages_number
+                number_of_pages_for_each_translator = int(pages_number/number_of_translators)
+                for i, j in enumerate(range(1, pages_number+1, number_of_pages_for_each_translator)):
+                    for page in range(j, number_of_pages_for_each_translator + j):
+                        page_text = pdf_reader.pages[page-1].extract_text()
+                        page = Page(book=instance, page=page, original_content=page_text, translator=translators[i])
+                        page.save()
+
             elif instance.status == "translated":
                 book = get_object_or_404(InProgressBook, book=instance)
                 book.delete()
