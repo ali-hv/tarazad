2d1fdb056459752b0881932561a11e56fdd57297
ali-hv <alihv5000@gmail.com>
2023-11-02 16:20:23 +0330

    create signals for books models

--- books/signals/handlers.py
+++ books/signals/handlers.py
@@ -1,9 +1,18 @@
-from django.db.models.signals import post_save
+from django.shortcuts import get_object_or_404
+from books.models import Book, InProgressBook
+from django.db.models.signals import pre_save
 from django.dispatch import receiver
-from books.models import Book
 
 
-@receiver(post_save, sender=Book)
-def add_book_to_inprogressbooks(sender, **kwargs):
-    print(kwargs)
-    
+@receiver(pre_save, sender=Book)
+def add_book_to_inprogressbooks(sender, instance: Book, **kwargs):
+    if instance.id is None:
+        pass
+    else:
+        previous = Book.objects.get(id=instance.id)
+        if previous.status != instance.status:
+            if instance.status == "in-progress":
+                book = InProgressBook.objects.create(book=instance)
+            elif instance.status == "translated":
+                book = get_object_or_404(InProgressBook, book=instance)
+                book.delete()
