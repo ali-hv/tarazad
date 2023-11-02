cf98d1802030db977294e98e17361922dcafca71
ali-hv <alihv5000@gmail.com>
2023-11-02 12:52:22 +0330

    register InProgressBook in admin

--- books/admin.py
+++ books/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from books.models import Book
+from books.models import Book, InProgressBook
 
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ['name', 'status', 'pages_number', 'min_translators', ]
+
+
+@admin.register(InProgressBook)
+class InProgressBookAdmin(admin.ModelAdmin):
+    list_display = ['book', 'pages_left', ]
+
