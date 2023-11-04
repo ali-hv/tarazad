61e6523efffcab7a6abd650b27ffb6ac3c6f3909
ali-hv <alihv5000@gmail.com>
2023-11-04 13:10:27 +0330

    save translated markdown file in the Book object

--- books/scripts/export_book.py
+++ books/scripts/export_book.py
@@ -1,14 +1,27 @@
 from django.shortcuts import get_object_or_404
+from books.models import Book, Page
+from django.core.files import File
+import os.path
 
 
-def export_book(Page, InProgressBook, book):
+def export_book(book):
     pages = Page.objects.filter(book=book)
     text = ''
+
     for i in pages:
         text += f'{i.translated_content}\n'
 
-    with open('book.txt', 'w') as f:
+    file_name = f'{book.name}.md'
+
+    with open(file_name, 'w') as f:
         f.write(text)
 
-    obj = get_object_or_404(InProgressBook, book=book)
-    obj.delete()
+    obj = get_object_or_404(Book, id=book.id)
+
+    with open(file_name, 'rb') as file:
+        django_file = File(file)
+        obj.translated_md_file.save(os.path.basename(file_name), django_file)
+
+    obj.status = 'translated'
+
+    obj.save()
