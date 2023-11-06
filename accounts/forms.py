615c53fd35df75eceb6aeb9d405d9ae220a913ed
ali-hv <alihv5000@gmail.com>
2023-11-06 19:26:12 +0330

    create UserRegisterForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
 
 
 class UserLoginForm(forms.Form):
@@ -6,3 +8,39 @@ class UserLoginForm(forms.Form):
         attrs={'class': 'form-control', 'placeholder': 'نام کاربری'}))
     password = forms.CharField(widget=forms.PasswordInput(
         attrs={'class': 'form-control', 'placeholder': 'رمز عبور'}))
+
+
+class UserRegisterForm(forms.Form):
+    username = forms.CharField(max_length=150, widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'نام کاربری'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(
+        attrs={'class': 'form-control', 'placeholder': 'رمز عبور'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(
+        attrs={'class': 'form-control', 'placeholder': 'تکرار رمز عبور'}))
+    phone_number = forms.CharField(max_length=12, widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'شماره همراه'}))
+    email = forms.EmailField(widget=forms.EmailInput(
+        attrs={'class': 'form-control', 'placeholder': 'ایمیل'}))
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password and confirm_password and password != confirm_password:
+            raise forms.ValidationError("Passwords do not match")
+
+    def clean_password(self):
+        password = self.cleaned_data.get("password")
+        validate_password(password)
+        return password
+
+    def save(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username=self.cleaned_data['username'],
+            password=self.cleaned_data['password1'],
+            phone_number=self.cleaned_data['phone_number'],
+            email=self.cleaned_data['email'],
+        )
+        return user
