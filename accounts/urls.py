698159eded58abfacf1cb801b6da2b2d17388032
ali-hv <alihv5000@gmail.com>
2023-11-06 18:21:56 +0330

    add logout and register urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,6 @@ app_name = 'accounts'
 
 urlpatterns = [
     path("login/", views.login_page, name="login"),
+    path("logout/", views.logout_user, name="logout"),
+    path("register/", views.register_page, name="register"),
 ]
