4dacdfbb57b6de5a2a42fa0ed01df67d0dfd3da2
ali-hv <alihv5000@gmail.com>
2023-11-06 19:40:20 +0330

    create register and logout views

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,15 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
-from .forms import UserLoginForm
+from .forms import UserLoginForm, UserRegisterForm
+from .scripts.translate_errors import to_persian
 
 
 def login_page(request):
     if request.user.is_authenticated:
         return redirect('home:home_page')
+
     login_form = UserLoginForm()
+    form_error = None
     if request.method == 'POST':
         login_form = UserLoginForm(request.POST)
         if login_form.is_valid():
@@ -16,7 +19,28 @@ def login_page(request):
             if user is not None:
                 login(request, user)
                 return redirect('home:home_page')
-            else:
-                login_form.add_error('password', 'نام کاربری یا رمز عبور صحیح نیستند')
-    context = {'login_form': login_form}
+
+            form_error = 'نام کاربری یا رمز عبور صحیح نیستند'
+
+    context = {'login_form': login_form, 'error': form_error}
     return render(request, 'accounts/login.html', context)
+
+
+def register_page(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home:home_page')
+    else:
+        form = UserRegisterForm()
+
+    form_errors = to_persian(form)
+    context = {'register_form': form, 'register_form_errors': form_errors}
+    return render(request, 'accounts/register.html', context=context)
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('home:home_page')
