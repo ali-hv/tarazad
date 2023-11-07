8d189e3b17b107b6d7acc05bf4a9cee7e11d3e02
ali-hv <alihv5000@gmail.com>
2023-11-07 22:45:44 +0330

    create support view

--- home/views.py
+++ home/views.py
@@ -5,6 +5,10 @@ def home_page(request):
     return render(request, "home/index.html")
 
 
+def support(requests):
+    return render(requests, "home/support.html")
+
+
 def about_us(request):
     return render(request, "home/about_us.html")
 
