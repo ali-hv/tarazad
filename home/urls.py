7efbd078b10a103413d7e5b967753c086be7c761
ali-hv <alihv5000@gmail.com>
2023-11-07 22:44:57 +0330

    add support path to urls.py

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import home_page, about_us, contact_us
+from .views import home_page, support, about_us, contact_us
 
 app_name = "home"
 
 urlpatterns = [
     path("", home_page, name="home_page"),
+    path("support", support, name="support"),
     path("about-us", about_us, name="about_us"),
     path("contact-us", contact_us, name="contact_us"),
 ]
