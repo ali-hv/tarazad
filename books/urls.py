0b0145c096a69284fcf993075156142d5af08c62
ali-hv <alihv5000@gmail.com>
2023-11-07 16:28:41 +0330

    add 'add_translator' to urls

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import books_list
+from .views import books_list, add_translator
 
 app_name = "books"
 
 urlpatterns = [
-    path('list/', books_list, name='books_list')
+    path('list/', books_list, name='books_list'),
+    path('add_translator/<int:book_id>', add_translator, name='add_translator'),
 ]
