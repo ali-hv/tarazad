996ca6f0e502dd8cec053e01efc8277927b603ab
ali-hv <alihv5000@gmail.com>
2023-11-09 02:01:45 +0330

    add submit_translation url

--- books/urls.py
+++ books/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import books_list, add_translator
+from .views import books_list, add_translator, submit_translation
 
 app_name = "books"
 
 urlpatterns = [
     path('list/', books_list, name='books_list'),
     path('add_translator/<int:book_id>', add_translator, name='add_translator'),
+    path('submit_translation/<int:page_id>', submit_translation, name='submit_translation'),
 ]
