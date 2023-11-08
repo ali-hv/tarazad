895af419cbb0f746cafc637131db96c6e47bffe2
ali-hv <alihv5000@gmail.com>
2023-11-09 02:07:00 +0330

    add book_pages and translate_page urls to dashboard app

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import dashboard
+from .views import dashboard, book_pages, translate_page
 
 app_name = "dashboard"
 
 urlpatterns = [
     path('', dashboard, name='dashboard'),
+    path('<int:book_id>', book_pages, name='book_pages'),
+    path('translate/<int:page_id>', translate_page, name='translate_page'),
 ]
