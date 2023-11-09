5457af264d10b56e776d71667f6a6260460a9f1e
ali-hv <alihv5000@gmail.com>
2023-11-09 23:44:02 +0330

    add change-info url

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("login/", views.login_page, name="login"),
     path("logout/", views.logout_user, name="logout"),
     path("register/", views.register_page, name="register"),
+    path("change-info/", views.change_info, name="change_info"),
     path("change-password/", views.ChangePassword.as_view(), name="change_password"),
 ]
