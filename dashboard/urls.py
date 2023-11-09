b96cc48ca3e4038896de7fe999cb6c1c9fd9ffec
ali-hv <alihv5000@gmail.com>
2023-11-09 18:49:04 +0330

    add profile url

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import dashboard, book_pages, translate_page
+from .views import dashboard, profile, book_pages, translate_page
 
 app_name = "dashboard"
 
 urlpatterns = [
     path('', dashboard, name='dashboard'),
+    path('profile', profile, name='profile'),
     path('<int:book_id>', book_pages, name='book_pages'),
     path('translate/<int:page_id>', translate_page, name='translate_page'),
 ]
