8dac4ff50825cd7485723081cf6b8bf89af70f53
ali-hv <alihv5000@gmail.com>
2023-11-10 23:34:32 +0330

    remove additional comments

--- decorators.py
+++ decorators.py
@@ -10,8 +10,6 @@ def email_verified_required(view_func):
         if user.is_authenticated and not user.email_verified:
             # Redirect to a page indicating that email is not verified
             return redirect(reverse('accounts:not_verified', args=['email']))
-            # Alternatively, you can return an HttpResponse with a message
-            # return HttpResponse("Your email is not verified.")
         return view_func(request, *args, **kwargs)
     return wrapper
 
@@ -21,9 +19,7 @@ def identity_verified_required(view_func):
     def wrapper(request, *args, **kwargs):
         user = request.user
         if user.is_authenticated and not user.identity_verified:
-            # Redirect to a page indicating that email is not verified
+            # Redirect to a page indicating that identity is not verified
             return redirect(reverse('accounts:not_verified', args=['identity']))
-            # Alternatively, you can return an HttpResponse with a message
-            # return HttpResponse("Your email is not verified.")
         return view_func(request, *args, **kwargs)
     return wrapper
