1ff7af99994d4f26becaf9508da82a3dfebf42d3
ali-hv <alihv5000@gmail.com>
2023-11-14 20:33:13 +0330

    hash the password before save in admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,3 +5,7 @@ from .models import User
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ['username', 'get_full_name', 'identity_verified', ]
+
+    def save_model(self, request, obj, form, change):
+        obj.set_password(obj.password)
+        super().save_model(request, obj, form, change)
