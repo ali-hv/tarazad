20637ede224ee27ed0f2cb2c1a52bbc54d678f50
ali-hv <alihv5000@gmail.com>
2023-11-15 01:14:48 +0330

    create translators_list view

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
+from django.db.models import Q, Count, Sum, Avg
 from django.shortcuts import render
-
+from accounts.models import User
 
 def home_page(request):
     return render(request, "home/index.html")
@@ -15,3 +16,14 @@ def about_us(request):
 
 def contact_us(request):
     return render(request, "home/contact_us.html")
+
+
+def translators_list(request):
+    users = User.objects.annotate(
+        num_translated_books=Count('book_translators', filter=Q(book_translators__status='translated')),
+        num_translated_pages=Sum('book_translators__book_pages__is_reviewed',
+                                 filter=Q(page_translator__is_reviewed=True)),
+        avg_translation_accuracy=Avg('page_translator__translation_accuracy',
+                                     filter=Q(page_translator__is_reviewed=True))
+    ).order_by('-num_translated_books', '-num_translated_pages', '-avg_translation_accuracy').filter(is_active=True)
+    return render(request, "home/translators_list.html", {"users": users})
