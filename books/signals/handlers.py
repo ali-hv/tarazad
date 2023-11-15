e1326687b8087b0cbd0d63ff76f4a651e0c3937c
ali-hv <alihv5000@gmail.com>
2023-11-15 18:40:00 +0330

    remove pages after translation completed and change user's translation accuracy after page translated

--- books/signals/handlers.py
+++ books/signals/handlers.py
@@ -1,8 +1,8 @@
-from django.shortcuts import get_object_or_404
-from books.models import Book, InProgressBook, Page
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
+from django.shortcuts import get_object_or_404
 
+from books.models import Book, InProgressBook, Page
 from books.scripts.distribute_pages_to_translators import distribute
 from books.scripts.export_book import export_book
 from translators.models import Translator
@@ -27,6 +27,10 @@ def add_book_to_inprogressbooks(sender, instance: Book, **kwargs):
                 in_progress_book = get_object_or_404(InProgressBook, book=instance)
                 in_progress_book.delete()
 
+                pages = Page.objects.filter(book=instance)
+                for page in pages:
+                    page.delete()
+
 
 @receiver(pre_save, sender=Page)
 def decrease_pages_left(sender, instance: Page, **kwargs):
@@ -38,6 +42,12 @@ def decrease_pages_left(sender, instance: Page, **kwargs):
             if instance.is_reviewed:
                 translator = get_object_or_404(Translator, user=instance.translator)
                 translator.pages_translated += 1
+
+                if translator.translation_accuracy != 0:
+                    translator.translation_accuracy = (translator.translation_accuracy + instance.translation_accuracy) / 2
+                else:
+                    translator.translation_accuracy = instance.translation_accuracy
+
                 translator.save()
 
                 obj = get_object_or_404(InProgressBook, book=instance.book)
