3a9ee137eb735aabd3072fa02afa2105a383d1b3
ali-hv <alihv5000@gmail.com>
2023-11-15 16:26:16 +0330

    set default value for translation_accuracy field

--- translators/models.py
+++ translators/models.py
@@ -8,7 +8,7 @@ User = AUTH_USER_MODEL
 
 class Translator(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="user_translator")
-    translation_accuracy = models.PositiveIntegerField(MaxValueValidator(100), blank=True, null=True)
+    translation_accuracy = models.PositiveIntegerField(validators=[MaxValueValidator(100)], default=0)
     books_participated = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="books_participated",
                                            blank=True, null=True)
     pages_translated = models.ForeignKey(Page, on_delete=models.CASCADE, related_name="translator_pages",
