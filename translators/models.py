fb0c839c09f16754c20bb14c4badb8f78c3678e1
ali-hv <alihv5000@gmail.com>
2023-11-15 16:07:37 +0330

    create Translator model

--- translators/models.py
+++ translators/models.py
@@ -1,3 +1,15 @@
+from django.core.validators import MaxValueValidator
+from core.settings import AUTH_USER_MODEL
+from books.models import Book, Page
 from django.db import models
 
-# Create your models here.
+User = AUTH_USER_MODEL
+
+
+class Translator(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="user_translator")
+    translation_accuracy = models.PositiveIntegerField(MaxValueValidator(100), blank=True, null=True)
+    books_participated = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="books_participated",
+                                           blank=True, null=True)
+    pages_translated = models.ForeignKey(Page, on_delete=models.CASCADE, related_name="translator_pages",
+                                         blank=True, null=True)
