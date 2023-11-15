2725b421eefe9bec459998704152838437634e5a
ali-hv <alihv5000@gmail.com>
2023-11-15 17:53:16 +0330

    remove additional methods from User model

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import Sum
 from django.templatetags.static import static
 from django.db import models
 
@@ -19,17 +18,3 @@ class User(AbstractUser):
         if self.first_name or self.last_name:
             return f"{self.first_name} {self.last_name}"
         return self.username
-
-    def get_number_of_participated_books(self):
-        return self.book_translators.filter(status='translated').count()
-
-    def get_number_of_translated_pages(self):
-        return self.page_translator.filter(is_reviewed=True).count()
-
-    def get_average_translation_accuracy(self):
-        translated_pages = self.page_translator.filter(is_reviewed=True)
-        total_accuracy = sum(page.translation_accuracy for page in translated_pages) if translated_pages else 0
-
-        average_accuracy = total_accuracy / translated_pages.count() if translated_pages.count() > 0 else 0
-        return round(average_accuracy * 10, 2)
-
