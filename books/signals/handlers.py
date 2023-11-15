2cf776f2a455fc318b91b813ef2ba42285612998
ali-hv <alihv5000@gmail.com>
2023-11-15 17:10:45 +0330

    increase pages_translated and books_participated values

--- books/signals/handlers.py
+++ books/signals/handlers.py
@@ -5,6 +5,7 @@ from django.dispatch import receiver
 
 from books.scripts.distribute_pages_to_translators import distribute
 from books.scripts.export_book import export_book
+from translators.models import Translator
 
 
 @receiver(pre_save, sender=Book)
@@ -12,14 +13,19 @@ def add_book_to_inprogressbooks(sender, instance: Book, **kwargs):
     if instance.id is None:
         pass
     else:
-        previous = Book.objects.get(id=instance.id)
+        previous = get_object_or_404(Book, id=instance.id)
         if previous.status != instance.status:
             if instance.status == "in-progress":
                 distribute(instance)
 
             elif instance.status == "translated":
-                book = get_object_or_404(InProgressBook, book=instance)
-                book.delete()
+                for translator in instance.translators.all():
+                    obj = get_object_or_404(Translator, user=translator)
+                    obj.books_participated += 1
+                    obj.save()
+
+                in_progress_book = get_object_or_404(InProgressBook, book=instance)
+                in_progress_book.delete()
 
 
 @receiver(pre_save, sender=Page)
@@ -30,6 +36,10 @@ def decrease_pages_left(sender, instance: Page, **kwargs):
         previous = get_object_or_404(Page, id=instance.id)
         if previous.is_reviewed != instance.is_reviewed:
             if instance.is_reviewed:
+                translator = get_object_or_404(Translator, user=instance.translator)
+                translator.pages_translated += 1
+                translator.save()
+
                 obj = get_object_or_404(InProgressBook, book=instance.book)
                 if obj.pages_left == 1:
                     export_book(instance.book)
